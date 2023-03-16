31b3f0d2ad962f2bfbaba89e6ecfdb1b4dd3d1fb
Rafael Pereira <devrafalime@proton.me>
2023-03-16 20:03:09 -0300

    Pomdoro com opção de definir novo tempo

--- JS/script.js
+++ JS/script.js
@@ -1,68 +1,26 @@
-let sec = 0;
+let sec =0;
 let min = 50;
-let hrs = 0;
-let i = 0;
 let secField = document.getElementById("seconds");
 let minField = document.getElementById("minutes");
-let hrsField = document.getElementById("hours");
-const Interval = setInterval(count,1000)
-clearInterval(Interval)
-
-//Função para contar segundos
-//function count(){
-//    if (secField.textContent>0){
-//        sec--
-//        secField.innerHTML=sec;
-//    } else{
-//        clearInterval(count)
-//        start()
-//    }
-//}
-
-//function start(){
-    //if ((minField.textContent!==0)&&(secField.textContent==0)){
-        //min--;
-        //minField.innerHTML = min;
-        //sec = 60;
-        //secField.innerHTML = sec;
-
-    //}
-    //if (secField.textContent>0){
-        //setInterval(count, 1000)
-        //sec--
-        //secField.innerHTML = sec
-    //}
-
-
-
-
-//}
 
 
 function count(){
-    if((hrsField.textContent!==0)&&(minField.textContent==0)&&(secField.textContent==0)){
-        hrs--;
-        hrsField.innerHTML=hrs;
-        min = 59;
-        minField.innerHTML=min;
-        sec = 59;
-        secField.innerHTML=sec;
-    }else if ((hrsField.textContent==0)&&(minField.textContent!==0)&&(secField.textContent==0)){
+    if ((min>0)&&(sec==0)){
         min--;
         minField.innerHTML=min;
         sec = 59;
         secField.innerHTML=sec;
-    }else if(secField.textContent!==0){
+    }else if(sec>0){
         sec--;
         secField.innerHTML=sec;
-    }else if((hrsField.textContent==0)&&(minField.textContent==0)&&(secField.textContent==0)){
+    }else if((min==0)&&(sec==0)){
         alert("Acabou o tempo")
         clearInterval(Interval)
     }
 }
 
 function start(){
-    setInterval(count, 1000)
+    Interval = setInterval(count,1000)
 }
 
 function stopAlert(){
@@ -76,12 +34,44 @@ function stop(){
 }
 function reset(){
     stop
-    hrsField.innerHTML=0
     minField.innerHTML=50
     secField.innerHTML=0
-    hrs=0
     min=50
     sec=0
 }
 
+//Begin New Time Button Script
+const timebutton = document.getElementById('newTime');
+// const modal = document.querySelector("dialog");
+
+// function show(){
+//     modal.show()
+// }
+// function closePop(){
+//     modal.close()
+// }
+
+// timebutton.addEventListener('click',show);
+
+// const confirmButton = document.getElementById('confirmTime');
+
+// confirmButton.addEventListener('click',closePop)
+
+function newtime(){
+    sec = prompt("Novo tempo segundos")
+    min = prompt("Novo tempo minutos")
+
+    secField.innerHTML = sec
+    minField.innerHTML = min
+}
+
+
+
+
+
+
+
+
+
+timebutton.addEventListener('click',newtime);
 
